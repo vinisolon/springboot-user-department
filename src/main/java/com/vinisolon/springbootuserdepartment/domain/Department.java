13025d922000009a319c71024fbc6a3beb70ce08
Vinicius Solon <vini.97@live.com>
2022-01-04 20:13:32 -0300

    Mapeamento JPA das classes de dominio

--- src/main/java/com/vinisolon/springbootuserdepartment/domain/Department.java
+++ src/main/java/com/vinisolon/springbootuserdepartment/domain/Department.java
@@ -8,14 +8,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "tb_department")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_user_id")
+    private User user;
 }
--- src/main/java/com/vinisolon/springbootuserdepartment/domain/User.java
+++ src/main/java/com/vinisolon/springbootuserdepartment/domain/User.java
@@ -8,15 +8,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+
+    @OneToMany(mappedBy = "id.department")
+    private Department department;
 }
