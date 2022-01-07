3fc149a517ac008fb13c4b239bee6563e0d58bdd
Vinicius Solon <vini.97@live.com>
2022-01-07 07:06:48 -0300

    Implementado DTO e classes de conversão #não finalizado

---
+++ src/main/java/com/vinisolon/springbootuserdepartment/dto/UserDepartmentDTO.java
@@ -0,0 +1,12 @@
+package com.vinisolon.springbootuserdepartment.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDepartmentDTO {
+    private Long id;
+    private String email;
+    private String password;
+    private Long departmentId;
+    private String departmentName;
+}
--- src/main/java/com/vinisolon/springbootuserdepartment/services/UserService.java
+++ src/main/java/com/vinisolon/springbootuserdepartment/services/UserService.java
@@ -1,6 +1,8 @@
 package com.vinisolon.springbootuserdepartment.services;
 
+import com.vinisolon.springbootuserdepartment.domain.Department;
 import com.vinisolon.springbootuserdepartment.domain.User;
+import com.vinisolon.springbootuserdepartment.dto.UserDepartmentDTO;
 import com.vinisolon.springbootuserdepartment.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +28,27 @@ public class UserService {
 
     public User insert(User obj) {
         return repo.save(obj);
+    }
+
+    private UserDepartmentDTO entityToDto(User user) {
+        UserDepartmentDTO dto = new UserDepartmentDTO();
+        dto.setId(user.getId());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        dto.setDepartmentId(user.getDepartment().getId());
+        dto.setDepartmentName(user.getDepartment().getName());
+        return dto;
+    }
+
+    private User entityToDto(UserDepartmentDTO dto) {
+        User user = new User();
+        Department department = new Department();
+        user.setId(dto.getId());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        department.setId(dto.getDepartmentId());
+        department.setName(dto.getDepartmentName());
+        user.setDepartment(department);
+        return user;
     }
 }
